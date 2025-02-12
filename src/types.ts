d1755ff4badde855827cdd3068ebfc6a5bfc11cd
Kibalnik Sergey <kibalnikserg@yandex.ru>
2025-02-12 11:05:32 +0300

    -_-

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-// types/index.ts
-export interface Book {
-  id: string;
-  title: string;
-  author: string;
-  imageUrl: string | null;  // Может быть null, если изображения нет
-  description: string;
-}
